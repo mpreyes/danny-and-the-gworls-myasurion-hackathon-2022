6b62e1d45a39b7f0b0e3fdac5ac2241464287a47
Jenaba Sow <jenabasow@gmail.com>
2022-11-10 14:44:01 -0500

    configure api

--- asurion_aspen_web/aspen_api/models.py
+++ asurion_aspen_web/aspen_api/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=60)
+    url = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
