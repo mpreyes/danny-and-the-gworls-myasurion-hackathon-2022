d8145e75ef3e6696fd4258fe38cb3993e1658929
Grace Harders <grace.harders@asurion.com>
2022-11-11 14:05:32 -0700

    more changes

--- asurion_aspen_web/aspen_api/views.py
+++ asurion_aspen_web/aspen_api/views.py
@@ -5,7 +5,8 @@ from .serializers import TeamSerializer
 from .models import Team
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import json
+import ast
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all().order_by('name')
     serializer_class = TeamSerializer
@@ -13,6 +14,14 @@ class TeamViewSet(viewsets.ModelViewSet):
 
 def aspen(request):
     team_list = Team.objects.all()
+    for i in enumerate(team_list):
+        index, team = i
+        repos = ast.literal_eval(ast.literal_eval(team.repositories_owned))
+        for r in repos:
+            repo_info = r.split(',')
+            repo_name = repo_info[0].split(":")[1]
+            repo_url = repo_info[1].split(":")[1]
+            team_list[index].repositories_owned = {repo_name: repo_url}
     context = {
         "team_list": team_list
     }
