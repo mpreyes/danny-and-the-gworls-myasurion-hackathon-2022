df244d786f73394225d62dd5e29b91c24503d9b9
mpreyes <reyes.madelyn.mr@gmail.com>
2022-11-11 12:25:41 -0600

    more stuffs

--- asurion_aspen_web/aspen_api/models.py
+++ asurion_aspen_web/aspen_api/models.py
@@ -8,10 +8,12 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=60)
-    url = models.CharField(max_length=60)
     tech_lead = models.CharField(max_length=60, default="")
     design_lead = models.CharField(max_length=60, default="")
+    product_lead = models.CharField(max_length=60, default="")
     mission_statement = models.TextField(default="")
-
+    repositories_owned = models.TextField(default="")
+    updates =  models.TextField(default="")
+    
     def __str__(self):
         return self.name
