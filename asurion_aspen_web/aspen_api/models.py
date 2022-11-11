c7631574071f29b58620ad24dc1c2a84830006d9
Grace Harders <grace.harders@asurion.com>
2022-11-11 09:05:13 -0700

    started the model type

--- asurion_aspen_web/aspen_api/models.py
+++ asurion_aspen_web/aspen_api/models.py
@@ -9,6 +9,9 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=60)
     url = models.CharField(max_length=60)
+    tech_lead = models.CharField(max_length=60)
+    design_lead = models.CharField(max_length=60)
+    mission_statement = models.TextField()
 
     def __str__(self):
         return self.name
